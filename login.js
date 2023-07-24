82e2f646c1a66352fc1c92867e5663e6ff96f1e4
0RutherFord0 <adityatwd0@gmail.com>
2023-07-24 12:46:11 +0530

    Added handleRegister()

--- login.js
+++ login.js
@@ -23,3 +23,27 @@ async function handleLogin() {
       "An error occurred during login.";
   }
 }
+
+// Function to handle registration
+async function handleRegister() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+
+    // Display the registration message
+    document.getElementById("message").innerText = data.message;
+  } catch (error) {
+    console.error("Error during registration:", error);
+    document.getElementById("message").innerText =
+      "An error occurred during registration.";
+  }
+}
